247edb65bed9e2da5f908b304f1cc768c717bd4f
iamagf <agf0710@gmail.com>
2022-11-12 00:35:22 -0600

    First step validators view

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,7 @@ import logo from './assets/img/logo_circle_background.png';
 function Header() {
   return (
     <div className="header">
-      <img className="header-logo" id="logo" src={logo} alt="logo" />
+      <Link to={"/"} className="header-button"><img className="header-logo" id="logo" src={logo} alt="logo" /></Link>
       <div className="header-nav">
         <Link to={"/chains"} className="header-button">Chains</Link>
         <Link to={"/validators"} className="header-button">Validators</Link>
--- src/views/Validators.jsx
+++ src/views/Validators.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Header from "../Header";
+import huahua from '.././assets/img/chain_huahua.png';
+import umee from '.././assets/img/chain_umee.png';
 
 function Validators() {
   return (
     <div className="validators">
-      <>
-        <Header />
-        <h1>Validators</h1>
-      </>
+      <Header />
+      <div className="validators-title">
+        Know our validators
+      </div>
+      <div className="validators-validators">
+        <a target="_blank" href="https://ping.pub/chihuahua/staking/chihuahuavaloper1ntfpx32g5hzvnlaqyv5kxggzgjtw2dj2gt708j"><img className="chain-logo" src={huahua} alt="huahua" /></a>
+        <a target="_blank" href="https://ping.pub/umee/staking/umeevaloper13k2hn8335n0e6qalsn2zs6pqpp24z8akqjlmnc"><img className="chain-logo" src={umee} alt="umee" /></a>
+      </div>
     </div>
   );
 }
