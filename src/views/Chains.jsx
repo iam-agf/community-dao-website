20e7e667e8cd6095bc9cc178fea6af36ed5cd487
iamagf <agf0710@gmail.com>
2022-11-12 01:16:33 -0600

    First Chains view added

--- src/views/Chains.jsx
+++ src/views/Chains.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from "../Header";
 import "./../sass/main.scss";
+import huahua from '.././assets/img/chain_huahua.png';
+import umee from '.././assets/img/chain_umee.png';
 
 function Chains() {
   return (
     <div className="chains">
       <Header />
-      Chains
+      <div className="chains-title">
+        Discover the chains
+      </div>
+      <div className="chains-validators">
+        <img className="chain-logo" src={huahua} alt="huahua" />
+        <img className="chain-logo" src={umee} alt="umee" />
+      </div>
     </div>
   );
 }
