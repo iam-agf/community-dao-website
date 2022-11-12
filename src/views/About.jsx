a3b94fc4252102314b64a952ca0ee9d466520ab3
iamagf <agf0710@gmail.com>
2022-11-12 01:16:12 -0600

    About cards edited

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -17,11 +17,23 @@ function About() {
       <div className="about-title">
         Meet the team
       </div>
+      <div className="about-description">
+        The Chihuahua DAO is composed by devoted people that work hard each day to help the Chihuahua Chain keep a healthy community. Click on them to know more.
+      </div>
       <div className="about-members">
-      <a target="_blank" href="https://twitter.com/Karan_patell1"><img className="about-members-member" src={karan} alt="karan" /></a>
-        <a target="_blank" href="https://twitter.com/Welding_Cosmos"><img className="about-members-member" src={nick} alt="nick" /></a>
-        <a target="_blank" href="https://twitter.com/CallistoCosmo"><img className="about-members-member" src={callisto} alt="callisto" /></a>
-        <a target="_blank" href="https://twitter.com/cosmo_jorin"><img className="about-members-member" src={jorin} alt="jorin" /></a>
+        <div className="about-members-card">
+          <a target="_blank" href="https://twitter.com/Karan_patell1"><img className="about-members-member" src={karan} alt="karan" /></a>
+        </div>
+        <div className="about-members-card">
+          <a target="_blank" href="https://twitter.com/Welding_Cosmos"><img className="about-members-member" src={nick} alt="nick" /></a>
+        </div>
+        <div className="about-members-card">
+          <a target="_blank" href="https://twitter.com/CallistoCosmo"><img className="about-members-member" src={callisto} alt="callisto" /></a>
+        </div>
+        <div className="about-members-card">
+          <a target="_blank" href="https://twitter.com/cosmo_jorin"><img className="about-members-member" src={jorin} alt="jorin" /></a>
+
+        </div>
       </div>
       <div className="about-title">
         Follow us =)
