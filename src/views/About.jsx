6198044069e19362d9cb3722e2d229f75c978d7d
iamagf <agf0710@gmail.com>
2022-11-12 00:34:47 -0600

    First step about view

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -5,6 +5,10 @@ import twitter from "../assets/img/twitter.png";
 import reddit from "../assets/img/reddit.png";
 import discord from "../assets/img/discord.png";
 import telegram from "../assets/img/telegram.png";
+import callisto from "../assets/img/callisto.jpeg";
+import karan from "../assets/img/karan.jpeg";
+import jorin from "../assets/img/jorin.jpeg";
+import nick from "../assets/img/nick.jpeg";
 
 function About() {
   return (
@@ -14,6 +18,10 @@ function About() {
         Meet the team
       </div>
       <div className="about-members">
+      <a target="_blank" href="https://twitter.com/Karan_patell1"><img className="about-members-member" src={karan} alt="karan" /></a>
+        <a target="_blank" href="https://twitter.com/Welding_Cosmos"><img className="about-members-member" src={nick} alt="nick" /></a>
+        <a target="_blank" href="https://twitter.com/CallistoCosmo"><img className="about-members-member" src={callisto} alt="callisto" /></a>
+        <a target="_blank" href="https://twitter.com/cosmo_jorin"><img className="about-members-member" src={jorin} alt="jorin" /></a>
       </div>
       <div className="about-title">
         Follow us =)
